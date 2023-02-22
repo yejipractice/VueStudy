c2c4ad03184926daef70e9547095aec20af44ad0
yejipractice <twoye1193@ajou.ac.kr>
2023-02-22 12:34:14 +0900

    TodoList 할일 나열: v-for

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul>
+            <!-- v-for 에서 key 설정 안해주면 eslint error 발생 -->
+           <li v-for="todoItem, key in todoItems" :key="key">{{ todoItem }}</li> 
+        </ul>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            todoItems: []
+        }
+    }, 
+    // 라이프 사이클 훅
+    created () {
+        if(localStorage.length>0) {
+            for(var i = 0; i < localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        console.log(this.todoItems);
+        }
+    }
 }
 </script>
 
