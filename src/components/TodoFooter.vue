b200af125c4461691cd9f1551d2afbd67be431ab
yejipractice <twoye1193@ajou.ac.kr>
2023-02-22 12:55:14 +0900

    TodoFooter 할일 모두 삭제 버튼 추가

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,12 +1,31 @@
+<!-- 모두 삭제하기 버튼을 포함하는 TodoFooter 컴포넌트 -->
 <template>
-    <div>footer</div>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        clearTodo() {
+            localStorage.clear();
+        }
+    }
 }
 </script>
 
 <style>
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBrn {
+        color: #e20303;
+        display: black;
+    }
 </style>
